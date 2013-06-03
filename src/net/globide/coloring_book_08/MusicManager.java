fa8319e708ad1adb3faa6d7479ef2926ee6ed063
bfs5030@gmail.com <bfs5030@gmail.com>
2013-06-03 23:31:13 +0000

    Added source files and updated common project files.
    The source files contain almost the entirety of the Coloring Book app.
    There are a few things that would need to be implemeneted (or
    alternatively, removed) for a clean build. So as of this commit,
    the application isn't automatically buildable when the repository is
    cloned. This will need to be addressed.
    
    The common project files have been updated to reflect additional
    license requirements.
    
    Signed-off-by: Byron Sanchez <bfs5030@gmail.com>

--- src/net/globide/coloring_book_08/MusicManager.java
+++ src/net/globide/coloring_book_08/MusicManager.java
@@ -1,22 +1,10 @@
 /*
- * Copyright (c) 2013 Byron Sanchez
- * www.chompix.com
- *
- * This file is part of "Coloring Book for Android."
- *
- * "Coloring Book for Android" is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version
- * 3 of the License, or (at your option) any later version.
- *
- * "Coloring Book for Android" is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with "Coloring Book for Android."  If not, see
- * <http://www.gnu.org/licenses/>.
+ * MusicManager
+ * Copyright (c) 2009 Robert Green
+ * www.rbgrn.net
+ * www.batterypoweredgames.com
+ * 
+ * Modified by Byron Sanchez.
  */
 
 package net.globide.coloring_book_08;
