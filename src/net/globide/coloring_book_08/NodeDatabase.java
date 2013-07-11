83d0f072fc72ebf6a40d0171068c583ecc18b951
bfs5030@gmail.com <bfs5030@gmail.com>
2013-07-11 00:14:26 +0000

    Removed unnecessary import.
    This import was never used. It was intended to be used by a dynamic
    database-path getter. This is because different devices may have
    different database paths. However, a different approach to database
    installation has been taken. Instead of copying the .db file to this
    "dynamic" path (the path can vary by device), the database itself and
    all database updateds occur through execution of DDL statements via
    changescripts.
    
    Thus, this import is not necessary now, nor has it ever been in the
    past. It just happened to get committed.
    
    Signed-off-by: Byron Sanchez <bfs5030@gmail.com>

--- src/net/globide/coloring_book_08/NodeDatabase.java
+++ src/net/globide/coloring_book_08/NodeDatabase.java
@@ -43,7 +43,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Environment;
 
 /**
  * Handles all database requests made by the application.
